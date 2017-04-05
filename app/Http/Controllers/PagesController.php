7f2ea199463a2e7ef773addd91ccf8da68c8f81f
Andrei <a.pasat@voodoo-studio.ch>
2017-04-05 15:26:41 +0300

    Start home page

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrei
+ * Date: 05/04/2017
+ * Time: 15:22
+ */
+
+namespace App\Http\Controllers;
+
+
+
+class PagesController extends Controller {
+
+    public function index() {
+        return view('index');
+    }
+
+}
