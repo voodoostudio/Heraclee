c5a10c804d0e25226ef98809a3fdfb3d10592c29
Sergiu <s.finciuc@voodoo-studio.ch>
2017-04-06 14:45:19 +0300

    footer create

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -185,12 +185,92 @@
     </section>
 
     <footer>
-        <section class="top_footer_section">
-            Lots of links
-        </section>
-        <section class="bottom_footer_section">
-            Here is footer
-        </section>
+            <section class="top_footer_section">
+                <div class="container-fluid">
+                    <h3>Heraclee - Agence Immobilière Saint Tropez</h3>
+                    <div class="row">
+                        <div class="col-md-2">
+                            <ul>
+                                <li><a href="#">Gent immobilier de luxe Genève</a></li>
+                                <li><a href="#">Immobilier de luxe Genève</a></li>
+                                <li><a href="#">Appartement à vendre Genève</a></li>
+                                <li><a href="#">Villa à vendre Genève</a></li>
+                                <li><a href="#">Villa à vendre Cologny</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-2">
+                            <ul>
+                                <li><a href="#">Villa à vendre Colonge Bellerive</a></li>
+                                <li><a href="#">Villa à vendre Corsier</a></li>
+                                <li><a href="#">Villa à vendre Anières</a></li>
+                                <li><a href="#">Villa à vendre Vandoeuvres</a></li>
+                                <li><a href="#">Propriété à vendre Colonge Bellerive</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-2">
+                            <ul>
+                                <li><a href="#">Propriété à vendre Cologny</a></li>
+                                <li><a href="#">Propriété à vendre Corsier</a></li>
+                                <li><a href="#">Propriété à vendre Anières</a></li>
+                                <li><a href="#">Propriété à vendre Vandoeuvres</a></li>
+                                <li><a href="#">Maison à vendre Genève</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-2">
+                            <ul>
+                                <li><a href="#">Villa de luxe à vendre Genève</a></li>
+                                <li><a href="#">Appartements à vendre à Genève</a></li>
+                                <li><a href="#">Maisons et villas à vendre à Genève</a></li>
+                                <li><a href="#">Appartement à louer Genève</a></li>
+                                <li><a href="#">Appartement à louer Vaud</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-2">
+                            <ul>
+                                <li><a href="#">Appartement à louer Valais</a></li>
+                                <li><a href="#">Appartement à vendre Vaud</a></li>
+                                <li><a href="#">Appartement à vendre Valais</a></li>
+                                <li><a href="#">Maison à vendre Genève</a></li>
+                                <li><a href="#">Maison à vendre Vaud</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-2">
+                            <ul>
+                                <li><a href="#">Chalet terrain Genève Valais</a></li>
+                                <li><a href="#">Immobilier de prestige Genève</a></li>
+                                <li><a href="#">Immobilier Genève</a></li>
+                                <li><a href="#">Immobilier Vaud</a></li>
+                                <li><a href="#">Immobilier Valais</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="bottom_footer_section">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut</p>
+                            <p class="copyright">© 2016 - Heraclee- Création et développement par voodoo studio</p>
+                        </div>
+                        <div class="col-md-4">
+                            <h4>Heraclee - Agence Immobilière Saint Tropez</h4>
+                            <div class="contact_block">
+                                <a href="/"><img src="/img/logo.svg"></a>
+                                <ul>
+                                    <li>+33 (0)4 94 54 20 01</li>
+                                    <li>44, Route des Plages<br>83990 Saint-Tropez</li>
+                                </ul>
+                            </div>
+                            <div class="input_container light">
+                                <input type="text" placeholder="Inscrivez-vous à notre newsletter">
+                                <button type="submit"><i class="icn icon-send_message" aria-hidden="true"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
     </footer>
 
     <script type="text/javascript" src="/js/libraries/jquery-3.2.0.min.js"></script>
