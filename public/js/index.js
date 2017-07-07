1622dde0b4e1bd60288a22948d4dd57abd2c9b2a
IC <himac!>
2017-07-07 15:25:04 +0200

    homepage carousel modified. Made the same as on results page

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
-    $('.carousel').slick({
-    //$('.results_carousel').slick({
+    $('.results_carousel').slick({
         infinite: true,
         slidesToShow: 2,
         slidesToScroll: 1,
         prevArrow: '<button type="button" class="slick-prev"><i class="icn icon-arrow_big_left"></i></button>',
         nextArrow: '<button type="button" class="slick-next"><i class="icn icon-arrow_big_right"></i></button>',
-        //autoplay: true,
-
+        autoplay: true,
         responsive: [
             {
                 breakpoint: 576,
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,164 +16,21 @@
 
     @include('includes.search_block_index')
 
-    <section class="carousel_offers_section">
-        <div class="container-fluid">
-            <h2>Nos derniers biens</h2>
-            <div class="offers_carousel carousel">
-            {{-- @foreach($properties as $property)
-                @if($property['step'] == 1)
-                    @include('includes.results_object')
-                @endif
-            @endforeach--}}
-                <div class="object_block">
-                    <div class="object_img_container">
-                        <a href="#"><img src="/img/objects/object_1.png"></a>
-                        <div class="loading"><i class="icn icon-more"></i></div>
-                    </div>
-                    <div class="object_info_container">
-                        <div class="object_info">
-                            <a href="#">Saint-Tropez</a>
-                            <div class="subtitle">
-                                <span class="city">Vanades</span>
-                                <span class="price">3 000 000 000 €</span>
-                            </div>
-                            <div class="properties_block">
-                                <ul class="properties">
-                                    <li>
-                                        <span class="icn_container"><i class="icn icon-area"></i></span>
-                                        <span class="prop_title">300 m</span><sup>2</sup>
-                                    </li>
-                                    <li>
-                                        <span class="icn_container"><i class="icn icon-rooms"></i></span>
-                                        <span class="prop_title">8</span>
-                                    </li>
-                                    <li>
-                                        <span class="icn_container"><i class="icn icon-bedroom"></i></span>
-                                        <span class="prop_title">5</span>
-                                    </li>
-                                    <li>
-                                    <span class="property_container">
-                                        <span class="icn_container tooltip" title="Dégagée Jardin Mer"><i class="icn icon-window_view"></i></span>
-                                        <span class="prop_val">Dégagée Jardin Mer</span>
-                                    </span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="object_block">
-                    <div class="object_img_container">
-                        <a href="#"><img src="/img/objects/object_2.png"></a>
-                        <div class="loading"><i class="icn icon-more"></i></div>
-                    </div>
-                    <div class="object_info_container">
-                        <div class="object_info">
-                            <a href="#">Ramatuelle</a>
-                            <div class="subtitle">
-                                <span class="city">Les Marres</span>
-                                <span class="price">2 500 000 000 €</span>
-                            </div>
-                            <div class="properties_block">
-                                <ul class="properties">
-                                    <li>
-                                        <span class="icn_container"><i class="icn icon-area"></i></span>
-                                        <span class="prop_title">200 m</span><sup>2</sup>
-                                    </li>
-                                    <li>
-                                        <span class="icn_container"><i class="icn icon-rooms"></i></span>
-                                        <span class="prop_title">7</span>
-                                    </li>
-                                    <li>
-                                        <span class="icn_container"><i class="icn icon-bedroom"></i></span>
-                                        <span class="prop_title">4</span>
-                                    </li>
-                                    <li>
-                                    <span class="property_container">
-                                        <span class="icn_container tooltip" title="Dégagée Jardin Mer"><i class="icn icon-window_view"></i></span>
-                                        <span class="prop_val">Dégagée Jardin Mer</span>
-                                    </span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="object_block">
-                    <div class="object_img_container">
-                        <a href="#"><img src="/img/objects/object_3.png"></a>
-                        <div class="loading"><i class="icn icon-more"></i></div>
-                    </div>
-                    <div class="object_info_container">
-                        <div class="object_info">
-                            <a href="#">VILLA DES PARCS</a>
-                            <div class="subtitle">
-                                <span class="city">Saint-Tropez</span>
-                                <span class="price">Prix sur demande</span>
-                            </div>
-                            <div class="properties_block">
-                                <ul class="properties">
-                                    <li>
-                                        <span class="icn_container"><i class="icn icon-area"></i></span>
-                                        <span class="prop_title">400 m</span><sup>2</sup>
-                                    </li>
-                                    <li>
-                                        <span class="icn_container"><i class="icn icon-rooms"></i></span>
-                                        <span class="prop_title">12</span>
-                                    </li>
-                                    <li>
-                                        <span class="icn_container"><i class="icn icon-bedroom"></i></span>
-                                        <span class="prop_title">6</span>
-                                    </li>
-                                    <li>
-                                    <span class="property_container">
-                                        <span class="icn_container tooltip" title="Dégagée Jardin Mer"><i class="icn icon-window_view"></i></span>
-                                        <span class="prop_val">Dégagée Jardin Mer</span>
-                                    </span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="object_block">
-                    <div class="object_img_container">
-                        <a href="#"><img src="/img/objects/object_4.png"></a>
-                        <div class="loading"><i class="icn icon-more"></i></div>
-                    </div>
-                    <div class="object_info_container">
-                        <div class="object_info">
-                            <a href="#">VILLAGE</a>
-                            <div class="subtitle">
-                                <span class="city">Saint-Tropez</span>
-                                <span class="price">5 625 €</span>
-                            </div>
-                            <div class="properties_block">
-                                <ul class="properties">
-                                    <li>
-                                        <span class="icn_container"><i class="icn icon-area"></i></span>
-                                        <span class="prop_title">380 m</span><sup>2</sup>
-                                    </li>
-                                    <li>
-                                        <span class="icn_container"><i class="icn icon-rooms"></i></span>
-                                        <span class="prop_title">2</span>
-                                    </li>
-                                    <li>
-                                        <span class="icn_container"><i class="icn icon-bedroom"></i></span>
-                                        <span class="prop_title">1</span>
-                                    </li>
-                                    <li>
-                                        <span class="icn_container tooltip" title="Dégagée Jardin Mer"><i class="icn icon-window_view"></i></span>
-                                        <span class="prop_val">Dégagée Jardin Mer</span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+
+    <section class="results_section">
+        <div class="results_container map_view">
+            <div class="container-fluid">
+                <div class="results_carousel row">
+                    @foreach($properties as $property)
+                        @if($property['step'] == 1)
+                            @include('includes.results_object')
+                        @endif
+                    @endforeach
                 </div>
             </div>
         </div>
     </section>
+
 @endsection
 
 @section('javascript')
