c6abd009bc0499a8584e927c06f780d56c353cbe
IC <himac!>
2017-10-26 12:22:31 +0300

    hide gallery fix started

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'Details page')
 @section('css')
-    <link rel="stylesheet" type="text/css" href="/css/news.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/dashboard.min.css">
 @stop
 @php
     $lang = LaravelLocalization::getCurrentLocaleRegional();
