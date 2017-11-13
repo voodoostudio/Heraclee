50560fa4665d786a9dac2e32f6fbd65ea2f8aa69
Alex <a.loscinin@voodoo-studio.ch>
2017-11-13 12:21:11 +0200

    added link in gallery

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -811,7 +811,17 @@ class PagesController extends Controller
     {
         $news = Posts::find($id);
 
-        return view('news_details', ['item' => $news]);
+        /* Last update on site */
+        $gallery_date = date('d.m.Y', strtotime(Gallery::orderBy('updated_at', 'desc')->value('updated_at')));
+        $gallery_last_date = (!empty($gallery_date)) ? $gallery_date : '';
+        $posts_date = date('d.m.Y', strtotime(Posts::orderBy('updated_at', 'desc')->value('updated_at')));
+        $posts_last_date = (!empty($posts_date)) ? $posts_date : '';
+        $properties_date = date('d.m.Y', strtotime(DB::table('apimo_properties')->select('updated_at')->orderBy('updated_at', 'desc')->value('updated_at')));
+        $properties_last_date = (!empty($properties_date)) ? $properties_date : '';
+
+        $last_update = max($posts_last_date, $gallery_last_date, $properties_last_date);
+
+        return view('news_details', ['item' => $news, 'last_update' => $last_update]);
     }
 
     /**
