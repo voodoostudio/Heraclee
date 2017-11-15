c954dfd40690a23ab349ce4660d7f685e0b7e453
IC <himac!>
2017-11-15 14:07:11 +0200

    added "new" label

--- public/js/custom_scripts/index.js
+++ public/js/custom_scripts/index.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         slidesToScroll: 1,
         prevArrow: '<button type="button" class="slick-prev"><i class="icn icon-arrow_big_left"></i></button>',
         nextArrow: '<button type="button" class="slick-next"><i class="icn icon-arrow_big_right"></i></button>',
-        // autoplay: true,
+        autoplay: true,
         responsive: [
             {
                 breakpoint: 576,
