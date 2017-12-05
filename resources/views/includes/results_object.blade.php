096b77274a3d1b3d0afba7c13c3af9dcfdd1ccb8
Alex <a.loscinin@voodoo-studio.ch>
2017-12-05 11:42:30 +0200

    add updated date

--- resources/views/includes/results_object.blade.php
+++ resources/views/includes/results_object.blade.php
@@ -31,24 +31,29 @@
             @endif
 
             @php
-                $date = new DateTime($property['created_at']);
+                $date_created = new DateTime($property['created_at']);
+                $date_updated = new DateTime($property['updated_at']);
                 $now = new DateTime();
             @endphp
-            @if($date->diff($now)->format("%m") < 3 && $date->diff($now)->format("%y") == 0 ||
-               ( $property['property_id'] == '1797932' || $property['property_id'] == '1672949' ||
-                 $property['property_id'] == '1775694' || $property['property_id'] == '1660917' ||
-                 $property['property_id'] == '1657855'
-               )
-            )
+
             {{--<ul class="creation_date">--}}
-                {{--<li><b>{{ trans('lang.created_at') }}</b>  {{ date('d.m.Y', strtotime($property['created_at'])) }}</li>--}}
-                {{--<li><b>{{ trans('lang.updated_at') }}</b>  {{ date('d.m.Y', strtotime($property['updated_at'])) }}</li>--}}
+            {{--<li><b>{{ trans('lang.created_at') }}</b>  {{ date('d.m.Y', strtotime($property['created_at'])) }}</li>--}}
+            {{--<li><b>{{ trans('lang.updated_at') }}</b>  {{ date('d.m.Y', strtotime($property['updated_at'])) }}</li>--}}
             {{--</ul>--}}
             <div class="new_label">
-                <span>{{ trans('lang.new') }}</span>
-                {{--<span>{{ trans('lang.updated') }}</span>--}}
+                @if($date_created->diff($now)->format("%m") < 3 && $date_created->diff($now)->format("%y") == 0 ||
+                    ( $property['property_id'] == '1797932' || $property['property_id'] == '1672949' ||
+                      $property['property_id'] == '1775694' || $property['property_id'] == '1660917' ||
+                      $property['property_id'] == '1657855'
+                    )
+               )
+                    <span>{{ trans('lang.new') }}</span>
+                @endif
+
+                @if($date_updated->diff($now)->format("%m") < 3 && $date_updated->diff($now)->format("%y") == 0)
+                    <span>{{ trans('lang.updated') }}</span>
+                @endif
             </div>
-            @endif
 
             @php
                 $link = ($property['category']['reference'] == 1) || ($property['category']['reference'] == 4) || ($property['category']['reference'] == 5) || ($property['category']['reference'] == 6) ?  route('details') . '?id=' . $property['property_id'] . ((!empty($slider)) ? $slider : '') : route('locationsDetails') . '?id=' . $property['property_id'] . ((!empty($slider)) ? $slider : '');
