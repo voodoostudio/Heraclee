469f3cee1da402be24262de673a3d45e4c5f9ebb
IC <himac!>
2017-12-27 18:58:17 +0300

    Newsletters scrolling optimized

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -33,7 +33,7 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-12 col-sm-6 offset-md-2 col-md-4 offset-lg-3 col-lg-3 margin_bottom_20">
+                    <div class="col-12 col-sm-6 col-md-4 margin_bottom_20">
                         <a href="{{ URL::to($lang . '/admin/gallery') }}">
                             <div class="outer_block_container">
                                 <div class="inner_block_container">
@@ -42,11 +42,20 @@
                             </div>
                         </a>
                     </div>
-                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 margin_bottom_20">
+                    <div class="col-12 col-sm-6 col-md-4 margin_bottom_20">
                         <a href="{{ URL::to($lang . '/admin/posts') }}">
                             <div class="outer_block_container">
                                 <div class="inner_block_container">
                                     <h2>News</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 margin_bottom_20">
+                        <a href="{{ URL::to($lang . '/admin/posts') }}">
+                            <div class="outer_block_container">
+                                <div class="inner_block_container">
+                                    <h2>Newsletters</h2>
                                 </div>
                             </div>
                         </a>
