d1e5f63bc84d847ccc6d97e7da30924f603edcdb
IC <himac!>
2018-02-22 12:10:56 +0200

    Label lang position fixed for admin

--- public/js/index.965824c7243562c72935733369927ce3.js
+++ public/js/index.965824c7243562c72935733369927ce3.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
         slidesToScroll: 1,
         prevArrow: '<button type="button" class="slick-prev"><i class="icn icon-arrow_big_left"></i></button>',
         nextArrow: '<button type="button" class="slick-next"><i class="icn icon-arrow_big_right"></i></button>',
-        autoplay: false,
+        autoplay: true,
         responsive: [
             {
                 breakpoint: 576,
